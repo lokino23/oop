7c9204f930707be672f2b96e5159169a44fcf841
Ruzaqir <ruzaqir@gmail.com>
2020-07-31 05:41:40 +0700

    kenapa eror ???

--- ape.php
+++ ape.php
@@ -2,5 +2,13 @@
 class Ape extends Animal
 {
     public $suara = "Auooo"
-    
+
+    function __construct($name)
+    {
+        $this->name = $name ;
+    }
+    function yell() 
+    {
+        echo$this->sounds;
+    }
 }
