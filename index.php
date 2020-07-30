0681391bd0c664fbdd49a9c45acf24de4c0f8d85
Ruzaqir <ruzaqir@gmail.com>
2020-07-31 01:05:44 +0700

    versi 1.2

--- index.php
+++ index.php
@@ -6,14 +6,17 @@ require "ape.php";
 $sheep = new Animal("shaun");
 
 echo $sheep->name; // "shaun"
+echo "<br>";
 echo $sheep->legs; // 2
-echo $sheep->cold_blooded // false
+echo "<br>" ;
+echo $sheep->cold_blooded; // false
 
 // NB: Boleh juga menggunakan method get (get_name(), get_legs(), get_cold_blooded())
 
-// index.php
+// ape.php
 $sungokong = new Ape("kera sakti");
 $sungokong->yell(); // "Auooo"
 
+//tentang frog.php
 $kodok = new Frog("buduk");
 $kodok->jump() ; // "hop hop"
